1ade07ffabcc0a5be5923cf742f568c500ed55dd
manarnoldi <manarnoldi@gmail.com>
2019-09-04 14:27:39 +0300

    Stared Adding Context

---
+++ NHAMIS.APP/Models/NHAMISContect.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NHAMIS.APP.Models
+{
+    public class NHAMISContect: IdentityDbContext
+    {
+        public DbSet<Nomination> Nominations;
+       
+    }
+}

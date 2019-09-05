fa024ad5df6e461b58963e0517dfaa34d7b41ef7
samuelmayenga <sammy.mayenga@gmail.com>
2019-09-05 12:12:55 +0300

    Routine

--- NHAMIS.APP/Controllers/HomeController.cs
+++ NHAMIS.APP/Controllers/HomeController.cs
@@ -13,6 +13,89 @@ namespace NHAMIS.APP.Controllers
             return View();
         }
 
+        public ActionResult Citation()
+        {
+            return View();
+
+        }
+
+        public ActionResult NominatingBody()
+        {
+            return View();
+
+
+        }
+        
+
+             public ActionResult Attachment()
+            {
+            return View();
+
+
+        }
+        public ActionResult OtherDetails()
+        {
+            return View();
+
+
+        }
+        
+        public ActionResult Medals()
+        {
+            return View();
+
+
+        }
+        
+        public ActionResult RoleOfHonor()
+        {
+            return View();
+
+
+        }
+
+        public ActionResult County()
+        {
+            return View();
+
+
+        }
+
+        public ActionResult SubCounty()
+        {
+            return View();
+
+
+        }
+        public ActionResult AcademicQual()
+        {
+            return View();
+
+
+        }
+
+        public ActionResult Ward()
+        {
+            return View();
+
+
+        }
+        
+
+         public ActionResult NominatingBodies()
+        {
+            return View();
+
+
+        }
+        public ActionResult NominationPeriod()
+        {
+            return View();
+
+
+        }
+
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
